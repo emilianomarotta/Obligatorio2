002a40b49bc77213ab5f3e248d7bdd2daf61162d
unknown <sebastianborjas2000@gmail.com>
2022-10-26 18:22:54 -0300

    Tabla Funcionario centrada

--- src/interfazGrafica/VentanaRegistrarFuncionario.java
+++ src/interfazGrafica/VentanaRegistrarFuncionario.java
@@ -79,7 +79,7 @@ public class VentanaRegistrarFuncionario extends javax.swing.JFrame {
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.Integer.class, java.lang.Integer.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
                 false, false, false
