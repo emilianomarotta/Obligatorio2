051a29a73c4e2f10eb547378b2853e8791838f5b
unknown <sebastianborjas2000@gmail.com>
2022-11-03 18:05:47 -0300

    ventana de ingreso/egreso

--- src/interfazGrafica/VentanaIngresoEgresoCarga.java
+++ src/interfazGrafica/VentanaIngresoEgresoCarga.java
@@ -27,6 +27,7 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
         initComponents();
         actualizarPantalla();
         crearGrilla();
+        
     }
 
     public int getAreaActual() {
@@ -65,9 +66,20 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
         lblArea = new javax.swing.JLabel();
         btnAreaAnterior = new javax.swing.JButton();
         btnAreaSiguiente = new javax.swing.JButton();
-        panelCargas = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        panelIngresarEgresar = new javax.swing.JPanel();
+        panelIngreso = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        panelEgreso = new javax.swing.JPanel();
+        lblTitulo = new javax.swing.JLabel();
+        lblTituloArticulo = new javax.swing.JLabel();
         lblArticulo = new javax.swing.JLabel();
+        lblTituloCodigo = new javax.swing.JLabel();
+        lblCodigo = new javax.swing.JLabel();
+        lblTituloCantidad = new javax.swing.JLabel();
+        lblTituloFuncionario = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        btnEgresar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -92,35 +104,106 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
             }
         });
 
-        panelCargas.setBackground(new java.awt.Color(102, 255, 204));
+        panelIngresarEgresar.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setText("Ingreso/Egreso");
+        panelIngreso.setBackground(new java.awt.Color(51, 255, 51));
+        panelIngreso.setPreferredSize(new java.awt.Dimension(480, 257));
 
-        lblArticulo.setText("jLabel2");
+        jLabel3.setText("Ingreso");
 
-        javax.swing.GroupLayout panelCargasLayout = new javax.swing.GroupLayout(panelCargas);
-        panelCargas.setLayout(panelCargasLayout);
-        panelCargasLayout.setHorizontalGroup(
-            panelCargasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelCargasLayout.createSequentialGroup()
-                .addGroup(panelCargasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(panelCargasLayout.createSequentialGroup()
-                        .addGap(35, 35, 35)
-                        .addComponent(jLabel1))
-                    .addGroup(panelCargasLayout.createSequentialGroup()
-                        .addGap(56, 56, 56)
-                        .addComponent(lblArticulo)))
-                .addContainerGap(319, Short.MAX_VALUE))
-        );
-        panelCargasLayout.setVerticalGroup(
-            panelCargasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(panelCargasLayout.createSequentialGroup()
+        javax.swing.GroupLayout panelIngresoLayout = new javax.swing.GroupLayout(panelIngreso);
+        panelIngreso.setLayout(panelIngresoLayout);
+        panelIngresoLayout.setHorizontalGroup(
+            panelIngresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelIngresoLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(94, 94, 94)
-                .addComponent(lblArticulo)
-                .addContainerGap(106, Short.MAX_VALUE))
+                .addComponent(jLabel3)
+                .addContainerGap(429, Short.MAX_VALUE))
         );
+        panelIngresoLayout.setVerticalGroup(
+            panelIngresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelIngresoLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel3)
+                .addContainerGap(228, Short.MAX_VALUE))
+        );
+
+        panelIngresarEgresar.add(panelIngreso, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        panelEgreso.setBackground(new java.awt.Color(102, 153, 255));
+
+        lblTitulo.setText("Egreso");
+
+        lblTituloArticulo.setText("Articulo:");
+
+        lblTituloCodigo.setText("Código:");
+
+        lblTituloCantidad.setText("Cantidad");
+
+        lblTituloFuncionario.setText("Funcionario");
+
+        jLabel2.setText("0");
+
+        btnEgresar.setText("Egresar");
+
+        javax.swing.GroupLayout panelEgresoLayout = new javax.swing.GroupLayout(panelEgreso);
+        panelEgreso.setLayout(panelEgresoLayout);
+        panelEgresoLayout.setHorizontalGroup(
+            panelEgresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelEgresoLayout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addGroup(panelEgresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(panelEgresoLayout.createSequentialGroup()
+                        .addComponent(lblTituloCantidad)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel1))
+                    .addGroup(panelEgresoLayout.createSequentialGroup()
+                        .addComponent(lblTituloFuncionario)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel2))
+                    .addGroup(panelEgresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(panelEgresoLayout.createSequentialGroup()
+                            .addComponent(lblTituloCodigo)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(lblCodigo))
+                        .addGroup(panelEgresoLayout.createSequentialGroup()
+                            .addComponent(lblTituloArticulo)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(lblArticulo))
+                        .addComponent(lblTitulo)))
+                .addContainerGap(369, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panelEgresoLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnEgresar)
+                .addContainerGap())
+        );
+        panelEgresoLayout.setVerticalGroup(
+            panelEgresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panelEgresoLayout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(lblTitulo)
+                .addGap(22, 22, 22)
+                .addGroup(panelEgresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblTituloCodigo)
+                    .addComponent(lblCodigo))
+                .addGap(18, 18, 18)
+                .addGroup(panelEgresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblTituloArticulo)
+                    .addComponent(lblArticulo))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(panelEgresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblTituloCantidad)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(panelEgresoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblTituloFuncionario)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnEgresar)
+                .addContainerGap(54, Short.MAX_VALUE))
+        );
+
+        panelIngresarEgresar.add(panelEgreso, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -128,9 +211,6 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(96, 96, 96)
-                        .addComponent(lblArea))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(65, 65, 65)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -141,24 +221,31 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
                             .addComponent(panelEspacios, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(lblColumnas))
                         .addGap(18, 18, 18)
-                        .addComponent(panelCargas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(31, Short.MAX_VALUE))
+                        .addComponent(panelIngresarEgresar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(96, 96, 96)
+                        .addComponent(lblArea)))
+                .addContainerGap(33, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(21, 21, 21)
                 .addComponent(lblArea)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(lblColumnas, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(panelEspacios, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(panelCargas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(panelEspacios, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(panelIngresarEgresar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(2, 2, 2)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnAreaAnterior)
                     .addComponent(btnAreaSiguiente))
-                .addContainerGap(21, Short.MAX_VALUE))
+                .addContainerGap(117, Short.MAX_VALUE))
         );
 
         pack();
@@ -168,6 +255,8 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
         if (this.areaActual < 4) {
             this.areaActual++;
             actualizarPantalla();
+                        panelIngreso.setVisible(false);
+            panelEgreso.setVisible(true);
         }
 
     }//GEN-LAST:event_btnAreaSiguienteActionPerformed
@@ -176,6 +265,8 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
         if (this.areaActual > 0) {
             this.areaActual--;
             actualizarPantalla();
+            panelIngreso.setVisible(true);
+            panelEgreso.setVisible(false);
         }
     }//GEN-LAST:event_btnAreaAnteriorActionPerformed
 
@@ -183,12 +274,23 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAreaAnterior;
     private javax.swing.JButton btnAreaSiguiente;
+    private javax.swing.JButton btnEgresar;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel lblArea;
     private javax.swing.JLabel lblArticulo;
+    private javax.swing.JLabel lblCodigo;
     private javax.swing.JLabel lblColumnas;
-    private javax.swing.JPanel panelCargas;
+    private javax.swing.JLabel lblTitulo;
+    private javax.swing.JLabel lblTituloArticulo;
+    private javax.swing.JLabel lblTituloCantidad;
+    private javax.swing.JLabel lblTituloCodigo;
+    private javax.swing.JLabel lblTituloFuncionario;
+    private javax.swing.JPanel panelEgreso;
     private javax.swing.JPanel panelEspacios;
+    private javax.swing.JPanel panelIngresarEgresar;
+    private javax.swing.JPanel panelIngreso;
     // End of variables declaration//GEN-END:variables
 
     private class EspacioListener implements ActionListener {
@@ -199,7 +301,7 @@ public class VentanaIngresoEgresoCarga extends javax.swing.JFrame {
             String textoBoton[] = cual.getText().split(":");
             int fila = Integer.parseInt(textoBoton[0]) - 1;
             int columna = Integer.parseInt(textoBoton[1]) - 1;
-            lblArticulo.setText(sistema.getListaAreas()[0].getCargas()[fila][columna].getArticulo().getNombre());
+            lblTituloArticulo.setText(sistema.getListaAreas()[0].getCargas()[fila][columna].getArticulo().getNombre());
 // código a completar según el botón presionado
         }
     }
